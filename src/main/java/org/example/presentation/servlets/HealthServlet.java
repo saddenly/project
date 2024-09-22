6ec2e00ee0876ff43623c18e079c75a2a158789f
victoria_rusalovskaya <vika.rusalovskaya@gmail.com>
2024-09-22 22:05:56 +0400

    fix servlets' urls

--- src/main/java/org/example/presentation/servlets/HealthServlet.java
+++ src/main/java/org/example/presentation/servlets/HealthServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("api/v1/health")
+@WebServlet("/api/v1/health")
 public class HealthServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
